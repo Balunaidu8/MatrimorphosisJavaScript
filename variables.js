832879d521de9ad493f176d5cd0b544b553f4657
Balu <balu@gmail.com>
2025-09-22 10:51:49 +0530

    variables added

---
+++ variables.js
@@ -0,0 +1,12 @@
+let stuentName = "balu";
+var collegeVtuNo = 22403;
+const dateOfBirth = 19;
+let anotherName = "viggu";
+if (anotherName == "viggu") {
+  let studentName = "Vignesh";
+  var vtuno = 22954;
+  console.log(studentName);
+  console.log(collegeVtuNo);
+}
+console.log(stuentName);
+console.log(collegeVtuNo);
