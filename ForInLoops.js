be57e1490fab97e82badc3ac1f1e6434be8a8bd5
Balu <balu@gmail.com>
2025-09-22 10:37:46 +0530

    for in loop added

---
+++ ForInLoops.js
@@ -0,0 +1,14 @@
+let default_design = {
+  colour: "Balck",
+  size: 20,
+  height: 6,
+};
+let user_preference = {
+  colour: "red",
+  height: 18,
+};
+console.log("default design :", default_design);
+for (let key in default_design) {
+  default_design[key] = user_preference[key];
+}
+console.log("after adding user prefences design is :", default_design);
