b21b9418435f6e0b869f7eb613df00a6f827ed33
Balu <balu@gmail.com>
2025-09-26 11:00:42 +0530

    in sufficent amount added

--- classAndException/class&Exception2.js
+++ classAndException/class&Exception2.js
@@ -5,22 +5,27 @@ class TotalAmountInBag {
     this.price = initialAmount;
     this.minDiscount = 5;
   }
-  calculateFinalPrice(discount) {
+  calculateFinalPrice(productAmount, productDiscount) {
     try {
       if (this.price <= 0) {
         throw new Error("Price should be greater than zero");
-      } else if (discount <= this.minDiscount) {
+      } else if (productDiscount <= this.minDiscount) {
         throw new Error(
           `The discount should be greater than ${this.minDiscount}%`
         );
-      } else {
-        let discountAmount = this.price * (discount / 100);
+      } else if (this.price < productAmount)
+        throw new Error(
+          "You Bag do not have sufficent money to purchase the iteam"
+        );
+      else {
+        let discountAmount = productAmount * (productDiscount / 100);
         console.log("The discount amount is:", discountAmount);
-        let totalAmount = this.price - discountAmount;
+        let productTotalAmount = productAmount - discountAmount;
         console.log(
           "The total amount of the product after discount is:",
-          totalAmount
+          productTotalAmount
         );
+        let totalAmountInBag = this.price - productTotalAmount;
       }
     } catch (e) {
       console.error(e.message);
@@ -28,5 +33,5 @@ class TotalAmountInBag {
   }
 }
 let product = new TotalAmountInBag(10000);
-product.calculateFinalPrice(10);
-product.calculateFinalPrice(4);
+product.calculateFinalPrice(50000, 10);
+product.calculateFinalPrice(2000, 4);
