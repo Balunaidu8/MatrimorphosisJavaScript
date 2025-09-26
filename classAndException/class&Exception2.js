10fafabafb36d3c2b8bc17f9a60272e97e7950e1
Balu <balu@gmail.com>
2025-09-26 10:41:32 +0530

    2nd example of class and exception is added

---
+++ classAndException/class&Exception2.js
@@ -0,0 +1,32 @@
+class Discount {
+  price;
+  minDiscount;
+  constructor(initialAmount) {
+    this.price = initialAmount;
+    this.minDiscount = 5;
+  }
+  calculateFinalPrice(discount) {
+    try {
+      if (this.price <= 0) {
+        throw new Error("Price should be greater than zero");
+      } else if (discount <= this.minDiscount) {
+        throw new Error(
+          `The discount should be greater than ${this.minDiscount}%`
+        );
+      } else {
+        let discountAmount = this.price * (discount / 100);
+        console.log("The discount amount is:", discountAmount);
+        let totalAmount = this.price - discountAmount;
+        console.log(
+          "The total amount of the product after discount is:",
+          totalAmount
+        );
+      }
+    } catch (e) {
+      console.error(e.message);
+    }
+  }
+}
+let product = new Discount(10000);
+product.calculateFinalPrice(10);
+product.calculateFinalPrice(4);
